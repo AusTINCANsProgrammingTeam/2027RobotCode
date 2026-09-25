40002c29c727950ef9dd0b4149dbd2affb8e051e
Adam Vander Pas <azvanderpas@gmail.com>
2026-09-25 18:33:56 -0500

    Move Main out of robot folder

--- src/main/java/first/Main.java
+++ src/main/java/first/Main.java
@@ -2,8 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package first.robot;
+package first;
 
+import first.robot.Robot;
 import org.wpilib.framework.RobotBase;
 
 /**

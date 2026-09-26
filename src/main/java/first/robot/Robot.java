e6d7db2831643bde5e95892af74039e482176840
Adam Vander Pas <azvanderpas@gmail.com>
2026-09-26 10:06:36 -0500

    CI Check failed correctly, remove exception and java setup step

--- src/main/java/first/robot/Robot.java
+++ src/main/java/first/robot/Robot.java
@@ -58,7 +58,6 @@ public class Robot extends LoggedRobot {
     // and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    throw new RuntimeException();
   }
 
   /**
